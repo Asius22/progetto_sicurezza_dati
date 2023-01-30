ab8efe3c5fae80d71e3949e1df8ee9e56abcd82e
Asius22 <ciro_2000@live.it>
2023-01-30 12:06:45 +0100

    code raffined

--- src/js/app.js
+++ src/js/app.js
@@ -3,24 +3,6 @@ App = {
   contracts: {},
 
   init: async function () {
-    // Load pets.
-   /* $.getJSON("../pets.json", function (data) {
-      var petsRow = $("#petsRow");
-      var petTemplate = $("#petTemplate");
-
-      for (i = 0; i < data.length; i++) {
-        petTemplate.find(".panel-title").text(data[i].name);
-        petTemplate.find("img").attr("src", data[i].picture);
-        petTemplate.find(".pet-breed").text(data[i].breed);
-        petTemplate.find(".pet-age").text(data[i].age);
-        petTemplate.find(".pet-location").text(data[i].location);
-        petTemplate.find(".btn-adopt").attr("data-id", data[i].id);
-
-        petsRow.append(petTemplate.html())
-        ;
-      }
-    })*/
-    ;
 
     return await App.initWeb3();
   },
@@ -88,12 +70,12 @@ App = {
 
   //TODO rimuovere funzione poichè inutile per il nostro progetto/ui
   markAdopted: function () {
-    var adoptionInstance;
+    var myInstance;
 
     App.contracts.Adoption.deployed().then(function(instance) {
-      adoptionInstance = instance;
+      myInstance = instance;
 
-      return adoptionInstance.getAdopters.call();
+      return myInstance.getAdopters.call();
     }).then(function(adopters) {
       for (i = 0; i < adopters.length; i++) {
         if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
@@ -109,7 +91,7 @@ App = {
   addNewStudent: function (event) {
     event.preventDefault();
 
-    var adoptionInstance;
+    var myInstance;
 
     window.web3.eth.getAccounts(function(error, accounts) {
       if (error) {
@@ -117,13 +99,13 @@ App = {
       }
       var account = accounts[0];
       App.contracts.StudentList.deployed().then(function(instance) {
-        adoptionInstance = instance;
+        myInstance = instance;
         var studentId = document.getElementById("stdMatricola").value;
         var stdName = document.getElementById("stdName").value;
         var stdSurname = document.getElementById("stdName").value;
         console.log(studentId);
         if (App.validateField(studentId) && App.validateField(stdName) && App.validateField(stdSurname))
-            return adoptionInstance.addStudent(stdName, stdSurname, studentId, {from: account});
+            return myInstance.addStudent(stdName, stdSurname, studentId, {from: account});
         else
           window.alert("Tutti i campi del form AGGIUNGI STUDENTE devono essere compilati");
       }).then(function(result){
@@ -142,45 +124,6 @@ App = {
 
   validateField: function(value){
     return !(value == null || value == "");
-
-  },
-
-  //! cancellare anche questo se non mostriamo gli esami
-  handleNewExam: function (event) {
-    event.preventDefault();
-
-    var adoptionInstance;
-
-    window.web3.eth.getAccounts(function(error, accounts) {
-      if (error) {
-        console.log(error);
-      }
-      var account = accounts[0];
-      App.contracts.ExamList.deployed().then(function(instance) {
-        adoptionInstance = instance;
-        var exName = document.getElementById("exName").value;
-        var exId = document.getElementById("exId").value;
-        var exMark = document.getElementById("mark").value;
-        var studentId = document.getElementById("ex-studentId").value;
-        if (App.validateField(exName) && App.validateField(exId) && App.validateField(exMark) && App.validateField(studentId))
-          return adoptionInstance.addExam(exName, exId, exMark, studentId, {from: account});
-        else
-          window.alert("Tutti i campi del form AGGIUNGI ESAME devono essere compilati");
-
-      }).then(function(result) {
-        console.log(result);
-        window.alert("ESAME AGGIUNTO CON SUCCESSO");
-      }).catch(function(err) {
-        console.log(err.message);
-        if (err.message.includes("permesso"))
-           window.alert("non hai il permesso di eseguire quest'azione");
-        else if (err.message.includes("studente"))
-          window.alert("lo studente non esiste");
-        else
-          window.alert("esiste già un esame con questo codice");
-
-      });
-    });
   },
 };
 
